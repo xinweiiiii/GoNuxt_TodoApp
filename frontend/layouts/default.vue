7f14a7501f276421b7b2d8229a597dc55aab418d
xinweiiiii <43470166+xinweiiiii@users.noreply.github.com>
2020-12-16 23:08:02 +0800

    update

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <Nuxt />
+    <navbar/>
+  </div>
+</template>
+
+<script>
+import navbar from '@/layouts/navbar.vue'
+
+export default {
+  head: {
+        link: [
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Istok+Web:wght@400;700&family=Rubik:wght@300;400;500;700&display=swap' },
+            { rel: 'stylesheet', href: 'https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css' },
+        ],
+    },
+    data () {
+        return {
+            mobile: false,
+        }
+    },
+    components: {
+        navbar
+    },
+}
+</script>
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ frontend/layouts/navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Cool Beans</h1>
+    <b-navbar toggleable="lg" type="dark" variant="default" class="navbar-horizontal">
+        <b-navbar-brand href="#">Task Management Application</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="#">
+                        <i class="ni ni-favourite-28"></i>
+                        <span class="nav-link-inner--text d-lg-none">Discover</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="#">
+                        <i class="ni ni-notification-70"></i>
+                        <span class="nav-link-inner--text d-lg-none">Notification</span>
+                    </a>
+                </li>
+                <b-nav-item-dropdown right>
+                <template v-slot:button-content>
+                    <i class="ni ni-settings-gear-65"></i>
+                    <span class="nav-link-inner--text d-lg-none">Settings</span>
+                </template>
+                <b-dropdown-item href="#">Action</b-dropdown-item>
+                <b-dropdown-item href="#">Another action</b-dropdown-item>
+                    <b-dropdown-divider></b-dropdown-divider>
+                <b-dropdown-item href="#">Something else here</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-nav-form>
+
+        </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+    },
+    methods: {
+    },
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<style scoped>
+    @media screen and (min-width: 1024px) 34
+    
+        img {
+            max-height:3rem;
+        }
+        nav {
+            padding: 0 3rem;
+        }
+    }
+
+
+</style>
